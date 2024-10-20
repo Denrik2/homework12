76e6a9b2b40f49684f6153d29d3139f1310555d4
Denrik2 <ketovdany@yandex.ru>
2024-10-20 14:02:49 +0300

    дз12

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Author dostoevsky = new Author("Фёдор", "Достоевский");
         Author mayakovsky = new Author("Владимир", "Маяковский");
-        Book karamazov = new Book("Братья Карамазовы", 1879, dostoevsky.name + "" +dostoevsky.surname);
-        Book idiot = new Book("Идиот", 1867, dostoevsky.name + "" +dostoevsky.surname);
-        System.out.println("karamazov.author = " + karamazov.author);
+        Book karamazov = new Book("Братья Карамазовы", 1879, dostoevsky);
+        Book idiot = new Book("Идиот", 1867, dostoevsky);
         karamazov.setAge(1881);
     }
 }
